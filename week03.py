48fcf68b6c77c0a83f1e531f18d477c6b368c67c
202444004 <ywkim050206@gmail.com>
2025-03-24 18:34:30 +0900

    v1.5

--- week03.py
+++ week03.py
@@ -1,5 +1,18 @@
-groups = ['Hot', 'Seventeen', 'Blackpink', 'NJZ']
-ratings = [1,2,4,3,100]
+def myZip(l1, l2):
+    l3 = list()
+    length = 0
+    if len(l1) < len(l2):
+        length = len(l1)
+    else:
+        length = len(l2)
 
-group_rating = list(zip(groups), ratings)
+    for i in range(length):
+        l3.append((l1[i], l2[i]))
+    return l3
+
+group = ['BlackPink', 'Hot', 'Seventeen', 'NJZ']
+ratings = [1,2,4,3]
+#ratings = [1,2,4,3,100]
+
+group_rating = myZip(group,ratings)
 print(group_rating)
