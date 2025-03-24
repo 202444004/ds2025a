b0c5e713126c0b661ac0408b997ee9a3d530e30d
202444004 <ywkim050206@gmail.com>
2025-03-24 17:52:58 +0900

    v1.1

--- week03.py
+++ week03.py
@@ -1,10 +1,5 @@
-#l = [99, 0, -7, 0, 16]
+groups = ['Hot', 'Seventeen', 'Blackpink', 'NJZ']
+ratings = [1,2,4,3,100]
 
-#for i in range(len(l)):
-    #print(f"{l[i]}, {id(l[i])}")
-
-import array
-
-arr = array.array('f', [99, 0, -7, 0, 16])
-for i in range(len(arr)):
-    print(f"{arr[i]:3}, {id(arr[i])}")
+group_rating = list(zip(groups), ratings)
+print(group_rating)
--- week0303.py
+++ week0303.py
@@ -1,15 +1,5 @@
-def move_zeros(l):
-    zero_idx = 0
-    for i in range(len(l)):
-        n = l[i]
-        if n != 0:
-            l[zero_idx] = n
-            if [zero_idx] != i:
-                l[i] = 0
-
-            zero_idx = zero_idx + 1
-    return l
-
-l = [99, 0, -7, 0, 16]
-move_zeros(l)
-print(l)
+cities = ['Incheon', 'Seoul', 'Incheon', 'Incheon', 'Gwangju']
+cities = set(cities)    #set으로 중복 값 제거
+cities.add('Incheon')
+cities.add('Suwon')     #dictionary, set 순서 개념 없음
+print(cities)
