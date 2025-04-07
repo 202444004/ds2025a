42868570412b60a08a7c9e0c527fc69e5199c628
202444004 <ywkim050206@gmail.com>
2025-04-07 18:17:29 +0900

    5.4

--- week05.py
+++ week05.py
@@ -1,7 +1,15 @@
-s1 = []
-s1.append("data structure")
-s1.append("database")
-print(s1[-1]) #peek처럼 동작
-print(s1)
-print(s1.pop()) #pop
-print(s1)
+def is_valid_parenthese(expression : str) -> bool: # type hint str-> parameter data , bool-> type
+    stack = list()
+    for letter in expression:
+        if letter == "(":
+            stack.append(letter)
+        if letter == ")":
+            if len(stack) == 0:
+                return False
+            else:
+                stack.pop()
+    return len(stack) == 0
+
+print(is_valid_parenthese("(1+2))"))
+print(is_valid_parenthese("(1+2)"))
+print(is_valid_parenthese("((3*2)/2)"))
