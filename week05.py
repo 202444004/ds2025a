27c58ad24e83dd9a386f25c440165b0656ac8741
202444004 <ywkim050206@gmail.com>
2025-04-07 18:35:08 +0900

    5.5

--- week05.py
+++ week05.py
@@ -1,15 +1,22 @@
 def is_valid_parenthese(expression : str) -> bool: # type hint str-> parameter data , bool-> type
     stack = list()
+    brackets = {']':'[', '}':'{', ')':'('}
     for letter in expression:
-        if letter == "(":
+        if letter in brackets.values():
             stack.append(letter)
-        if letter == ")":
-            if len(stack) == 0:
+        if letter in brackets.keys():
+            if not stack or stack.pop() != brackets[letter]:
                 return False
-            else:
-                stack.pop()
-    return len(stack) == 0
+    return not stack
 
+#def check(exprs : str) -> bool:
+    #brackets = {'}':'{',')':'('}
+    #for brackets in exprs:
+        #if brackets.keys()== "}":
+
+print(is_valid_parenthese("[({1+2)]}"))
+print(is_valid_parenthese("[({1+2})]"))
+print(is_valid_parenthese(")1+2()"))
 print(is_valid_parenthese("(1+2))"))
 print(is_valid_parenthese("(1+2)"))
 print(is_valid_parenthese("((3*2)/2)"))
