6b71f15a3ba02eeb72d3eee2a05bf6149564e39a
202444004 <ywkim050206@gmail.com>
2025-04-08 22:35:10 +0900

    stack0.2

--- practice.py
+++ practice.py
@@ -1,20 +1,24 @@
-#STACK
+#STACK -> LinkedList
+
+class Node:     #스택 내부 링크드 리스트의 노드를 나타내는 클래스 정의
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
 class Stack:
     def __init__(self):
-        self.items = [] #instance 변수 items 정의, 리스트 초기화
+        self.top = None #top으로 사용할 instance 변수 정의
 
-    def push(self, data):
-        self.items.append(data) #append -> items에 새로운 데이터 추가
+    def push(self, data):   #새 노드를 만듦
+        node = Node(data)
+        if self.top is None: #링크드리스트에 top이 없으면
+            self.top = node #새 노드를 생성
+        else:   #top이 있으면
+            node.next = self.top #새 노드의 다음 요소에 기존 탑의 값을 할당
+            self.top = node #새 노드를 탑으로 만듦
 
     def pop(self):
-        return self.items.pop() #pop -> 가장 최근 추가된 요소(top) 반환 -> 삭제
-
-    def size(self):
-        return len(self.items) #len -> 스택의 길이 반환
-
-    def is_empty(self):
-        return len(self.items) == 0 #len == 0 이면 리스트가 비어있음
-
-    def peek(self):
-        return self.items[-1] #스택의 가장 마지막, 최근 추가된 요소(top) 반환
+        if self.top is None: #스택이 비어 있을때 팝을 시도하면 예외를 일으킴
+            raise IndexError('스택이 비었음.')
+        poppednode = self.top.next #스택에 요소가 있을때는 링크드리스트의 첫번째 요소를 제거
+        return poppednode.data #이를 반환
