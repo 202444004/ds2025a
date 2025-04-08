4bf2dc84dea8cc5f2d825eca68f7cc9bf95f60a8
202444004 <ywkim050206@gmail.com>
2025-04-09 08:56:59 +0900

    stack0.3

--- practice.py
+++ practice.py
@@ -1,24 +1,14 @@
-#STACK -> LinkedList
+#Stack-Reverse
+#1 #a_string[::-1]
+#2 #''.join(reversed('a_string'))
+#3  Stack 사용하기
+def reverse_string(a_string):   #매개변수: 문자열
+    stack = []  #
+    string = ""
+    for c in a_string:
+        stack.append(c) #각 문자를 스택에 추가
+    for c in a_string:
+        string += stack.pop()   #각 문자를 a_string에 추가
+    return string   #뒤집힌 문자열 반환
 
-class Node:     #스택 내부 링크드 리스트의 노드를 나타내는 클래스 정의
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-
-class Stack:
-    def __init__(self):
-        self.top = None #top으로 사용할 instance 변수 정의
-
-    def push(self, data):   #새 노드를 만듦
-        node = Node(data)
-        if self.top is None: #링크드리스트에 top이 없으면
-            self.top = node #새 노드를 생성
-        else:   #top이 있으면
-            node.next = self.top #새 노드의 다음 요소에 기존 탑의 값을 할당
-            self.top = node #새 노드를 탑으로 만듦
-
-    def pop(self):
-        if self.top is None: #스택이 비어 있을때 팝을 시도하면 예외를 일으킴
-            raise IndexError('스택이 비었음.')
-        poppednode = self.top.next #스택에 요소가 있을때는 링크드리스트의 첫번째 요소를 제거
-        return poppednode.data #이를 반환
+print(reverse_string("Biever"))
